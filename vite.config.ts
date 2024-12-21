b7bcf6134c9f932e14ee2f3b3a900663f6dae59e
TianLang Hacker <3071571983@qq.com>
2024-12-22 01:03:37 +0800

    6

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from 'tailwindcss'; 
+import tailwindcss from 'tailwindcss';
 
-
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/' : '/', // 根据部署路径调整
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
+    ...(mode === 'development' ? [vueDevTools()] : []), // 仅在开发模式启用
   ],
   css: {
     postcss: {
@@ -21,9 +19,25 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}));
 
-
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueJsx(),
+//     vueDevTools(),
+//   ],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
