0e85af5a3b23eaa4c4283f9a19ab8f76cadca167
TianLang Hacker <3071571983@qq.com>
2024-12-22 00:44:55 +0800

    6

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -7,6 +7,7 @@ export interface Product {
   price: number;
 }
 
+
 // 定义 CartState
 export interface CartState {
   items: Product[];
--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,37 +1,46 @@
 <template>
-    <div>
-      <h1>{{ product.name }}</h1>
-      <button @click="addToCart">Add to Cart</button>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue'
-  import { useCartStore } from '../stores/cart'
-  import axios from 'axios'
-  
-  interface Product {
-    id: number
-    name: string
-  }
-  
-  export default defineComponent({
-    data() {
-      return {
-        product: {} as Product
-      }
-    },
-    async created() {
-      const productId = this.$route.params.id
+  <div>
+    <h1>{{ product.name }}</h1>
+    <p>Price: {{ product.price }}</p>
+    <button @click="addToCart">Add to Cart</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useCartStore } from '../stores/cart'
+import axios from 'axios'
+
+interface Product {
+  id: number
+  name: string
+  price: number // 添加了 price 属性
+}
+
+export default defineComponent({
+  data() {
+    return {
+      product: {} as Product
+    }
+  },
+  async created() {
+    const productId = this.$route.params.id
+    try {
       const response = await axios.get<Product>(`/api/product/${productId}`)
       this.product = response.data
-    },
-    methods: {
-      addToCart() {
-        const cartStore = useCartStore()
-        cartStore.addToCart(this.product)
-      }
+    } catch (error) {
+      console.error('Failed to fetch product:', error)
     }
-  })
-  </script>
-  
+  },
+  methods: {
+    addToCart() {
+      const cartStore = useCartStore()
+      if (!this.product.price) {
+        console.error('Cannot add product to cart: price is missing.')
+        return
+      }
+      cartStore.addToCart(this.product)
+    }
+  }
+})
+</script>
